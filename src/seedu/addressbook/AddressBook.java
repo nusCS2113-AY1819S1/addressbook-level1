df783d2b94f894a25e4cfe406b20d4c1297d6134
jitwei98 <limjitwei@gmail.com>
2018-08-26 17:55:31 +0800

    AddressBook.java: extract method processUserCommand()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -211,11 +211,15 @@ public class AddressBook {
         processProgramArgs(args);
         loadDataFromStorage();
         while (true) {
-            String userCommand = getUserInput();
-            echoUserCommand(userCommand);
-            String feedback = executeCommand(userCommand);
-            showResultToUser(feedback);
+            processUserCommand();
         }
+    }
+
+    private static void processUserCommand() {
+        String userCommand = getUserInput();
+        echoUserCommand(userCommand);
+        String feedback = executeCommand(userCommand);
+        showResultToUser(feedback);
     }
 
     /*
