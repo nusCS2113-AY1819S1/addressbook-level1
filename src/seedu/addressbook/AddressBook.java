e6da15da9d8733a682cb148bdacababe2417da60
amirulmaricar <Amirul@comp.nus.edu.sg>
2018-09-07 20:22:07 +0800

    Pattern searchWord 1.0

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -14,14 +14,9 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * NOTE : =============================================================
@@ -473,7 +468,8 @@ public class AddressBook {
      * @return set of keywords as specified by args
      */
     private static Set<String> extractKeywordsFromFindPersonArgs(String findPersonCommandArgs) {
-        return new HashSet<>(splitByWhitespace(findPersonCommandArgs.trim()));
+        Pattern searchWord = Pattern.compile(findPersonCommandArgs, Pattern.CASE_INSENSITIVE);
+       return new HashSet<>(splitByWhitespace(findPersonCommandArgs.trim()));
     }
 
     /**
